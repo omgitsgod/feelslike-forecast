4b63f556f3e7eaf347abc4441fd9bc07a0e48a42
omgitsgod <omgitsgod@gmail.com>
2019-06-05 22:10:38 -0400

    Add links to homepage to test

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,9 @@
 import React, {useState} from 'react';
+import { Link } from 'react-router-dom'
 import './App.css';
 import Icon from '@mdi/react'
 import {mdiWeatherHurricane} from '@mdi/js'
+
 
 function Home() {
 
@@ -16,6 +18,10 @@ function Home() {
           spin
         />
         <h1> Welcome! </h1>
+        <Link to='/today' style={{ textDecoration: 'none', color: 'white'}}><p>Today</p></Link>
+        <Link to='/hourly' style={{ textDecoration: 'none', color: 'white'}}><p>Hourly</p></Link>
+        <Link to='/fiveday' style={{ textDecoration: 'none', color: 'white'}}><p>Five Day</p></Link>
+        <Link to='/map' style={{ textDecoration: 'none', color: 'white'}}><p>Map</p></Link>
       </header>
     </div>
 
