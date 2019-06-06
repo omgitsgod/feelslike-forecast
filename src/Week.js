dec8e97dc4804254c83f469eb8ae88edcbda7950
omgitsgod <omgitsgod@gmail.com>
2019-06-06 16:46:36 -0400

    Map out week forecast

--- src/Week.js
+++ src/Week.js
@@ -3,17 +3,40 @@ import './App.css';
 import Icon from '@mdi/react'
 import {mdiWeatherHurricane} from '@mdi/js'
 
-function Week() {
+function Week(props) {
+  let daily
+  if (props.daily) {
+    daily = props.daily.data.map(x => {
 
-  return (
-
-    <div className="Section">
-      <header className="Section-header">
-       <h1>Five Day Forecast</h1>
-      </header>
+    return (
+    <div>
+    <h5>Day</h5>
+    <p>High: {x.apparentTemperatureHigh}</p>
+    <p>Low: {x.apparentTemperatureLow}</p>
+    <p>{x.summary}</p>
     </div>
   )
+  })
+  }
+  console.log(props);
+  if (props.daily) {
+    return (
 
+      <div className="Section">
+        <header className="Section-header">
+          {daily}
+        </header>
+      </div>
+    )
+  } else {
+    return (
+      <div className="App">
+        <header className="App-header">
+      <p>loading...</p>
+      </header>
+      </div>
+    )
+  }
 }
 
 export default Week;
