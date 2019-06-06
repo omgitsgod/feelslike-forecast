f94ae13cf96d03a3bc69d6d86c8ffc0592d03a05
omgitsgod <omgitsgod@gmail.com>
2019-06-06 11:28:34 -0400

    Change today to currently

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, {useState} from 'react';
 import './App.css';
 import { Route  } from 'react-router-dom'
 import Home from './Home'
-import Today from './Today'
+import Currently from './Currently'
 import FiveDay from './FiveDay'
 import Hourly from './Hourly'
 import Map from './Map'
@@ -13,7 +13,7 @@ function App() {
   return (
     <div>
     <Route exact path="/" component={Home} />
-    <Route exact path="/today" component={Today} />
+    <Route exact path="/currently" component={Currently} />
     <Route exact path="/fiveday" component={FiveDay} />
     <Route exact path="/hourly" component={Hourly} />
     <Route exact path ="/map" component={Map} />
