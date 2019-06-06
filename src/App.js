314871cc51d61f86ffb683e21cce1129f0141a87
omgitsgod <omgitsgod@gmail.com>
2019-06-05 22:00:42 -0400

    Add empty hourly component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React, {useState} from 'react';
-
 import './App.css';
 import { Route  } from 'react-router-dom'
 import Home from './Home'
 import Today from './Today'
 import FiveDay from './FiveDay'
+import Hourly from './Hourly'
 
 function App() {
 
@@ -14,6 +14,7 @@ function App() {
     <Route exact path="/" component={Home} />
     <Route exact path="/today" component={Today} />
     <Route exact path="/fiveday" component={FiveDay} />
+    <Route exact path="/hourly" component={Hourly} />
     </div>
   )
 }
---
+++ src/Hourly.js
@@ -0,0 +1,17 @@
+import React, {useState} from 'react';
+import './App.css';
+import Icon from '@mdi/react'
+import {mdiWeatherHurricane} from '@mdi/js'
+
+function Hourly() {
+
+  return (
+
+    <div>
+
+    </div>
+  )
+
+}
+
+export default Hourly;
