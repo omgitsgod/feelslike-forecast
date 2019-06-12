05ea9c01668e46be85fd7a0cc45ae4ed35ae591b
omgitsgod <omgitsgod@gmail.com>
2019-06-12 15:59:37 -0400

    Fix section width

--- src/Week.js
+++ src/Week.js
@@ -2,15 +2,66 @@ import React, {useState} from 'react';
 import './App.css';
 import Icon from '@mdi/react'
 import {mdiWeatherHurricane} from '@mdi/js'
+import ReactAnimatedWeather from 'react-animated-weather';
 
 function Week(props) {
   let daily
+  const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday","Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+  let day = new Date().getDay()
+  const setIcon = (x) => {
+    switch(x) {
+      case 'clear-day':
+      return 'CLEAR_DAY'
+      break
+      case 'clear-night':
+      return 'CLEAR_NIGHT'
+      break
+      case 'partly-cloudy-day':
+      return 'PARTLY_CLOUDY_DAY'
+      break
+      case 'partly-cloudy-night':
+      return 'PARTLY_CLOUDY_NIGHT'
+      break
+      case 'cloudy':
+      return 'CLOUDY'
+      break
+      case 'rain':
+      return 'RAIN'
+      break
+      case 'sleet':
+      return 'SLEET'
+      break
+      case 'snow':
+      return 'SNOW'
+      break
+      case 'wind':
+      return 'WIND'
+      break
+      case 'fog':
+      return 'FOG'
+      break
+      default:
+      return 'CLEAR_DAY'
+      break
+    }
+  }
+  const defaults = {
+    color: '#61dafb',
+    size: 50,
+    animate: true
+  };
   if (props.daily) {
     daily = props.daily.data.map(x => {
 
     return (
     <div className="boxy">
-    <h5>Day</h5>
+    <h5>{days[day++]}</h5>
+    <ReactAnimatedWeather
+      icon={setIcon(x.icon)}
+      color={defaults.color}
+      size={defaults.size}
+      animate={defaults.animate}
+    />
     <p>High: {x.apparentTemperatureHigh}</p>
     <p>Low: {x.apparentTemperatureLow}</p>
     <p>{x.summary}</p>
