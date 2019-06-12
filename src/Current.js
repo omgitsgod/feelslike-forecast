ef145de4019342cf3dacb0de84b097aaa55606df
omgitsgod <omgitsgod@gmail.com>
2019-06-12 14:55:32 -0400

    Add icon package and render Current. Add dummy data so save hits

--- src/Current.js
+++ src/Current.js
@@ -2,6 +2,7 @@ import React, {useState} from 'react';
 import './App.css';
 import Icon from '@mdi/react'
 import {mdiWeatherHurricane} from '@mdi/js'
+import ReactAnimatedWeather from 'react-animated-weather';
 
 function Currently(props) {
   const [temp, setTemp] = useState(0)
@@ -11,8 +12,53 @@ function Currently(props) {
   const [stormDistance, setStormDistance] = useState(0)
   const [results, setResults] = useState({})
   let lat, long
+  let icon
+  if (props.currently) {
+    console.log(props.currently.icon)
+    switch(props.currently.icon) {
+      case 'clear-day':
+      icon='CLEAR_DAY'
+      break
+      case 'clear-night':
+      icon='CLEAR_NIGHT'
+      break
+      case 'partly-cloudy-day':
+      icon='PARTLY_CLOUDY_DAY'
+      break
+      case 'partly-cloudy-night':
+      icon='PARTLY_CLOUDY_NIGHT'
+      break
+      case 'cloudy':
+      icon='CLOUDY'
+      break
+      case 'rain':
+      icon='RAIN'
+      break
+      case 'sleet':
+      icon='SLEET'
+      break
+      case 'snow':
+      icon='SNOW'
+      break
+      case 'wind':
+      icon='WIND'
+      break
+      case 'fog':
+      icon='FOG'
+      break
+      default:
+      icon = 'CLEAR_DAY'
+      break
+    }
 
+  }
 
+  const defaults = {
+  icon: icon,
+  color: '#61dafb',
+  size: 100,
+  animate: true
+};
 
 
   if (props.currently) {
@@ -21,13 +67,15 @@ function Currently(props) {
     <div className="Section">
       <header className="Section-header">
 
-        <Icon path={mdiWeatherHurricane}
-          size={4}
-          color="#61dafb"
-          spin
-        />
+        
         <div className="boxy">
         <div className="inner">
+        <ReactAnimatedWeather
+          icon={defaults.icon}
+          color={defaults.color}
+          size={defaults.size}
+          animate={defaults.animate}
+        />
         <h1>Right now:</h1>
         <br />
         <h2>Temp: {props.currently.temperature}</h2>
