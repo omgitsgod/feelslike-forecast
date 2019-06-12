66d39752751eec15f623508dde6bde6bd85f5f50
omgitsgod <omgitsgod@gmail.com>
2019-06-12 15:04:13 -0400

    Add icons to Hourly component

--- src/Hourly.js
+++ src/Hourly.js
@@ -2,16 +2,65 @@ import React, {useState} from 'react';
 import './App.css';
 import Icon from '@mdi/react'
 import {mdiWeatherHurricane} from '@mdi/js'
+import ReactAnimatedWeather from 'react-animated-weather';
 
 function Hourly(props) {
   let hours
   let hour = 0
+  const setIcon = (x) => {
+    switch(x) {
+      case 'clear-day':
+      return 'CLEAR_DAY'
+      break
+      case 'clear-night':
+      return 'CLEAR_NIGHT'
+      break
+      case 'partly-cloudy-day':
+      return 'PARTLY_CLOUDY_DAY'
+      break
+      case 'partly-cloudy-night':
+      return 'PARTLY_CLOUDY_NIGHT'
+      break
+      case 'cloudy':
+      return 'CLOUDY'
+      break
+      case 'rain':
+      return 'RAIN'
+      break
+      case 'sleet':
+      return 'SLEET'
+      break
+      case 'snow':
+      return 'SNOW'
+      break
+      case 'wind':
+      return 'WIND'
+      break
+      case 'fog':
+      return 'FOG'
+      break
+      default:
+      return 'CLEAR_DAY'
+      break
+    }
+  }
+  const defaults = {
+    color: '#61dafb',
+    size: 50,
+    animate: true
+  };
   if (props.hourly) {
     hours = props.hourly.data.map(x => {
 
     return (
     <div className="boxy">
     <h5>{hour++} hour(s) from now</h5>
+    <ReactAnimatedWeather
+      icon={setIcon(x.icon)}
+      color={defaults.color}
+      size={defaults.size}
+      animate={defaults.animate}
+    />
     <p>Temp: {x.apparentTemperature}</p>
     <p>{x.summary}</p>
     </div>
