5ea50b07cca6f32b93abc00cbdbecb71217f535a
omgitsgod <omgitsgod@gmail.com>
2019-10-23 22:34:19 -0400

    Cleanup Map

--- src/Map.js
+++ src/Map.js
@@ -1,7 +1,5 @@
-import React, {useState} from 'react';
+import React from 'react';
 import './App.css';
-import Icon from '@mdi/react'
-import {mdiWeatherHurricane} from '@mdi/js'
 
 function Map() {
 
