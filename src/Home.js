0dd2271da05ff2af7d9a28d278fdb63842d7fffb
omgitsgod <omgitsgod@gmail.com>
2019-10-23 22:33:48 -0400

    Cleanup Home

--- src/Home.js
+++ src/Home.js
@@ -1,5 +1,4 @@
-import React, {useState} from 'react';
-import { Link } from 'react-router-dom'
+import React from 'react';
 import './App.css';
 import Icon from '@mdi/react'
 import {mdiWeatherHurricane} from '@mdi/js'
