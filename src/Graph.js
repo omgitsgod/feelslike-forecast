30eb448062c1edbbb204a2192829de45cf4a4163
Ryan <omgitsgod@gmail.com>
2020-07-08 23:39:27 -0400

    Refactor and fix formatting

--- src/Graph.js
+++ src/Graph.js
@@ -1,49 +1,69 @@
-import React, {useRef, useEffect} from 'react';
-import './css/Graph.css'
+import React, { useRef, useEffect } from 'react';
+import './css/Graph.css';
 import Chart from 'chart.js';
 
-function Graph(props) {
-  const chartRef = useRef(null)
-  Chart.defaults.global.defaultFontColor = 'white';
-  useEffect(()=>{
-   const myChartRef = chartRef.current.getContext('2d');
-   const pointBackgroundColors = []
-   const radiusArr = []
 
-   const x = new Chart(myChartRef, {
-      type: props.type,
+function Graph(props) {
+
+  const chartRef = useRef(null);
+  const { selected, data, label, type, x } = props;
+
+  useEffect(()=>{
+
+    const myChartRef = chartRef.current.getContext('2d');
+    const pointBackgroundColors = [];
+    const radiusArr = [];
+    const chart = new Chart(myChartRef, {
+      type: type,
       options: {
         responsive: true,
         maintainAspectRatio: false,
-        onClick: (e) => {console.log(e.target)}
+        scales: {
+          yAxes: [{
+            ticks: {
+              fontColor: 'white'
+            },
+          }],
+          xAxes: [{
+            ticks: {
+              fontColor: 'white'
+            },
+          }]
+        },
+        legend: {
+          labels: {
+            fontColor: 'white'
+          }
+        },
       },
       data: {
-        labels: props.x,
-        datasets: [
-          {
-            label: props.label,
-            data: props.data,
-            borderColor: '#61dafb',
-            backgroundColor: pointBackgroundColors,
-            radius: radiusArr,
-            pointHoverRadius: 10,
-            pointBackgroundColor: pointBackgroundColors,
-            fill: false,
-          }
-        ]
+        labels: x,
+        datasets: [{
+          label: label,
+          fontColor: 'white',
+          data: data,
+          borderColor: '#61dafb',
+          backgroundColor: pointBackgroundColors,
+          radius: radiusArr,
+          pointHoverRadius: 10,
+          pointBackgroundColor: pointBackgroundColors,
+          fill: false,
+        }]
       }
-    })
-  for (let i = 0; i < x.data.datasets[0].data.length; i++) {
-    if (i === props.selected) {
-        pointBackgroundColors.push("#61dafb");
-        radiusArr.push(5)
-    } else {
-        pointBackgroundColors.push("#25393e");
-        radiusArr.push(2)
+    });
+
+    for (let i = 0; i < data.length; i++) {
+      if (i === selected) {
+          pointBackgroundColors.push('#61dafb');
+          radiusArr.push(5);
+      } else {
+          pointBackgroundColors.push('#25393e');
+          radiusArr.push(2);
+      }
     }
-  }
-  x.update()
-}, [props.selected, props.data, props.label, props.type, props.x])
+
+    chart.update();
+}, [selected, data, label, type, x]);
 
 
   return (
@@ -53,7 +73,7 @@ function Graph(props) {
         ref={chartRef}
       />
     </div>
-  )
+  );
 }
 
 export default Graph;
