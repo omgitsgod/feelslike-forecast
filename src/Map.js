b7f64946c5486013cb1bc61624805ccd9f15b949
Ryan <omgitsgod@gmail.com>
2020-07-10 22:05:34 -0400

    Change double quotes to single quotes

--- src/Map.js
+++ src/Map.js
@@ -1,17 +1,16 @@
-import React from 'react';
-import './css/App.css';
+import React from "react";
+import "./css/App.css";
 
-function Map() {
+
+function Map(props) {
 
   return (
-
-    <div className="Section">
-      <header className="Section-header">
+    <div className='Section'>
+      <header className='Section-header'>
         <h1>Coming Soon!</h1>
       </header>
     </div>
-  )
-
+  );
 }
 
 export default Map;
