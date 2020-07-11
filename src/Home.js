bbb0ca352226b3a1f980766304c95796ca836f5f
Ryan <omgitsgod@gmail.com>
2020-07-10 22:07:52 -0400

    Refactor and fix formatting

--- src/Home.js
+++ src/Home.js
@@ -1,28 +1,26 @@
 import React from 'react';
 import './css/Home.css';
-import Icon from '@mdi/react'
-import {mdiWeatherHurricane} from '@mdi/js'
+import Icon from '@mdi/react';
+import { mdiWeatherHurricane } from '@mdi/js';
 
 
 function Home(props) {
+
+  const { daily } = props;
+
   return (
-
-    <div className="Section">
-      <div className="Section-div">
-
+    <div className='Section'>
+      <div className='Section-div'>
         <Icon path={mdiWeatherHurricane}
           size={4}
-          color="#61dafb"
+          color='#61dafb'
           spin
         />
         <h1> Welcome to Feelslike Forecast!</h1>
-        {props.daily ? <p>{props.daily.summary}</p> : <p className="loading">getting your weather</p>}
-
+        {daily ? <p>{daily.summary}</p> : <p className='loading'>getting your weather</p>}
       </div>
     </div>
-
-  )
-
+  );
 }
 
 export default Home;
