fe03f86d7318fb0b3f56702a379aaecb1f1dcec3
Ryan <omgitsgod@gmail.com>
2020-07-11 10:28:57 -0400

    Refactor and fix formatting

--- src/App.js
+++ src/App.js
@@ -1,38 +1,43 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import './css/App.css';
-import { Route  } from 'react-router-dom'
-import Home from './Home'
-import Sidebar from './Sidebar'
-import Current from './Current'
-import Week from './Week'
-import Hourly from './Hourly'
-import Map from './Map'
+import { Route  } from 'react-router-dom';
+import { getPosition } from './Utils';
+import Home from './Home';
+import Sidebar from './Sidebar';
+import Current from './Current';
+import Week from './Week';
+import Hourly from './Hourly';
+import Map from './Map';
+
 
 function App() {
-  const [results, setResults] = useState({})
+
+  const [results, setResults] = useState({});
+
+  const handleGeolocation = async () => {
+
+    const position = await getPosition();
+    const { latitude, longitude } = position.coords;
+    const json = await fetch(`https://feelslike-backend.herokuapp.com/get/${latitude}/${longitude}`).then(r => r.json());
+    
+    setResults(json);
+  }
 
   useEffect(() => {
-  new Promise(function (resolve, reject) {
-    navigator.geolocation.getCurrentPosition(resolve, reject)
-  })
-  .then((position) => {
-    fetch(`https://feelslike-backend.herokuapp.com/get/${position.coords.latitude}/${position.coords.longitude}`).then(r => r.json()).then(setResults)
-  })
-  .catch((err) => {
-    console.error(err.message);
-  });
-  }, [])
-  return (
-    <div className="Section-div">
-      <Sidebar />
-      <Route exact path="/" render={(props)=><Home {...props} daily={results.daily}/>}/>
-      <Route exact path="/current" render={(props)=><Current {...props} currently={results.currently}/>}/>
-      <Route exact path="/hourly" render={(props)=><Hourly {...props} hourly={results.hourly}/>}/>
-      <Route exact path="/week" render={(props)=><Week {...props} daily={results.daily}/>}/>
-      <Route exact path ="/map" component={Map} />
-    </div>
-  )
-}
+  
+    handleGeolocation();
+  }, []);
 
+  return (
+    <div className='Section-div'>
+      <Sidebar />
+      <Route exact path='/' render={(props)=><Home {...props} daily={results.daily} />} />
+      <Route exact path='/current' render={(props)=><Current {...props} currently={results.currently} />} />
+      <Route exact path='/hourly' render={(props)=><Hourly {...props} hourly={results.hourly} />} />
+      <Route exact path='/week' render={(props)=><Week {...props} daily={results.daily} />} />
+      <Route exact path ='/map' component={Map} />
+    </div>
+  );
+}
 
 export default App;
